a308ecf47e50b88f00ac1980e3ae49dc9cadfe72
Taka Okunishi <okunishinishi@gmail.com>
2017-05-31 13:50:05 +0900

    watch時にrequire.cacheを明示的に消すようにした

--- test/define_test.js
+++ test/define_test.js
@@ -7,6 +7,7 @@
 const define = require('../lib/define.js')
 const ponContext = require('pon-context')
 const { ok } = require('assert')
+const path = require('path')
 const asleep = require('asleep')
 const writeout = require('writeout')
 
@@ -28,6 +29,7 @@ describe('define', function () {
     let srcDir = `${__dirname}/../tmp/testing-watching/src`
     let destDir = `${__dirname}/../tmp/testing-watching/dest`
     let src = srcDir + '/foo.pcss'
+    require.cache[ path.resolve(srcDir, src) ] = { m: 'This is mock cache' }
     yield writeout(src, ':root { --red: #d33; } a { &:hover { color: color(var(--red) a(54%)); } }', { mkdirp: true })
     yield asleep(100)
     define(srcDir + '/*.*', (event, filename) => {
