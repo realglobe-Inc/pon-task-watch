f6fc187a785d91b1ee4dc4ddbeba85b823ecdb62
okunishinishi <okunishinishi@gmail.com>
2017-10-30 10:25:21 +0900

    Return close function

--- test/define_test.js
+++ test/define_test.js
@@ -6,40 +6,41 @@
 
 const define = require('../lib/define.js')
 const ponContext = require('pon-context')
-const { ok } = require('assert')
+const {ok} = require('assert')
 const path = require('path')
 const asleep = require('asleep')
 const writeout = require('writeout')
 
-const co = require('co')
-
 describe('define', function () {
   this.timeout(3000)
 
-  before(() => co(function * () {
+  before(async () => {
 
-  }))
+  })
 
-  after(() => co(function * () {
+  after(async () => {
 
-  }))
+  })
 
-  it('Define', () => co(function * () {
+  it('Define', async () => {
     let ctx = ponContext()
     let srcDir = `${__dirname}/../tmp/testing-watching/src`
     let destDir = `${__dirname}/../tmp/testing-watching/dest`
     let src = srcDir + '/foo.pcss'
-    require.cache[ path.resolve(srcDir, src) ] = { m: 'This is mock cache' }
-    yield writeout(src, ':root { --red: #d33; } a { &:hover { color: color(var(--red) a(54%)); } }', { mkdirp: true })
-    yield asleep(100)
-    define(srcDir + '/*.*', (event, filename) => {
-      writeout(destDir + '/' + filename, 'hogehoge', { mkdirp: true })
-    }, { delay: 1 })(ctx)
-    yield writeout(src, ':root { --red: #dd1; } a { &:hover { color: color(var(--red) a(54%)); } }', { mkdirp: true })
-    yield asleep(200)
-    yield writeout(src, ':root { --red: #5FF; } a { &:hover { color: color(var(--red) a(54%)); } }', { mkdirp: true })
-    yield asleep(200)
-  }))
+    require.cache[path.resolve(srcDir, src)] = {m: 'This is mock cache'}
+    await writeout(src, ':root { --red: #d33; } a { &:hover { color: color(var(--red) a(54%)); } }', {mkdirp: true})
+    await asleep(100)
+    const task = define(srcDir + '/*.*', (event, filename) => {
+      writeout(destDir + '/' + filename, 'hogehoge', {mkdirp: true})
+    }, {delay: 1})
+    const close = await task(ctx)
+    await writeout(src, ':root { --red: #dd1; } a { &:hover { color: color(var(--red) a(54%)); } }', {mkdirp: true})
+    await asleep(200)
+    await writeout(src, ':root { --red: #5FF; } a { &:hover { color: color(var(--red) a(54%)); } }', {mkdirp: true})
+    await asleep(200)
+
+    close()
+  })
 })
 
 /* global describe, before, after, it */
